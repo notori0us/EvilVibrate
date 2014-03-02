9e8872d5d0cc4a4e0b586400c9a594efdd989dd1
notori0us <cbwcjw@gmail.com>
2014-03-02 16:08:04 -0500

    added mootools class for android
    should be extensible in the future

--- EvilVibrate.js
+++ EvilVibrate.js
@@ -3,19 +3,22 @@ var EvilVibrate = new Class({
 	initialize: function(platform) {
 		this.platform = platform;
 
-		this.image = "images/"
-		this.sound = "sounds/"
+		this.image = "images/";
+		this.sound = "sounds/";
 
 		switch (platform) {
 			case "android4":
-				this.image += "android4"
-				this.sound += "android4"
+				this.image += "android.png";
+				this.sound += "Girtab.ogg.";
 			break;
 		}
-	}
+	},
 	
 
 	html: function() {
-		return ""
+		return '<img width="100%" src="' + this.image + '" onclick="window.location.href=\'tel:15135024072\';" />' + '<audio autoplay="autoplay">' + '<source src="' + this.sound + ' "</audio>';
+
 	}
-);
+});
+
+test = new EvilVibrate("android4");
