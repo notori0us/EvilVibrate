99f590ec58e825c6f2d3dfd3e87210139cc6e317
notori0us <cbwcjw@gmail.com>
2014-04-10 14:57:09 -0400

    updated with better image resizing

--- EvilVibrate.js
+++ EvilVibrate.js
@@ -1,4 +1,9 @@
-
+/*
+ * The EvilVibrate Class
+ *
+ * A proof of concept design to trick users int
+ *
+ */
 var EvilVibrate = new Class({
 	initialize: function(platform) {
 		this.platform = platform;
@@ -6,15 +11,17 @@ var EvilVibrate = new Class({
 		this.image = "images/";
 		this.sound = "sounds/";
 
-		switch (platform) {
+		switch (platform) {  // figure out our platform
 			case "android4":
 				this.image += "android.png";
 				this.sound += "Girtab.mp3";
 				break;
+
 			case "iphone":
 				this.image += "iphone.png";
 				this.sound += "marimba.mp3";
 				break;
+
 			case "winphone":
 				this.image += "windows.png";
 				this.sound += "windowsphone.mp3";
@@ -22,17 +29,19 @@ var EvilVibrate = new Class({
 		}
 	},
 
-
 	html: function() {
-//		return '<img width="100%" src="' + this.image + '" onclick="window.location.href=\'tel:15135024072\';" />' + '<audio autoplay="autoplay">' + '<source src="' + this.sound + ' "</audio>';
-		return '<img width="100%" src="' + this.image + '" onclick="window.location.href=\'http://www.amazon.com/gp/product/B000F1OS20/ref=as_li_ss_tl?ie=UTF8&camp=1789&creative=390957&creativeASIN=B000F1OS20&linkCode=as2&tag=custcompbui03-20\';" />' + '<audio autoplay="autoplay">' + '<source src="' + this.sound + ' "</audio>';
+		return '<img id="full-screen-background-image" width="100%" src="' + this.image + '" onclick="window.location.href=\'http://www.amazon.com/gp/product/B000F1OS20/ref=as_li_ss_tl?ie=UTF8&camp=1789&creative=390957&creativeASIN=B000F1OS20&linkCode=as2&tag=custcompbui03-20\';" />' + '<audio autoplay="autoplay">' + '<source src="' + this.sound + ' "</audio>';
 
-	}
+	},
+
+	vibrate: function () {
+		navigator.vibrate = navigator.vibrate ||
+			navigator.webkitVibrate ||
+			navigator.mozVibrate ||
+			navigator.msVibrate;
+
+		navigator.vibrate([1000, 500, 1000, 500, 1000, 500, 1000, 500, 1000, 500, 1000, 500, 1000, 500]);
+	},
 });
 
-navigator.vibrate = navigator.vibrate ||
-navigator.webkitVibrate ||
-navigator.mozVibrate ||
-navigator.msVibrate;
 
-navigator.vibrate([1000, 500, 1000, 500, 1000, 500, 1000, 500, 1000, 500, 1000, 500, 1000, 500]);
--- README.md
+++ README.md
@@ -3,6 +3,10 @@ EvilVibrate
 
 Code demonstrating evil use of the HTML5 vibrate tag/api
 
+Included here
+-------------
+
+
 Demo
 ----
 
